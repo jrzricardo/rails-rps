9dfdcba10187bb470a243356f693047f654bbad9
Ricardo Juarez <146899774+jrzricardo@users.noreply.github.com>
2024-06-06 20:56:15 +0000

    built homepage, altered layout to link to each move

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,8 +1,8 @@
 class GameController < ApplicationController
   def rock
-    moves = ["rock", "paper", "scissors"]
+    @moves = ["rock", "paper", "scissors"]
   
-    @comp_move = moves.sample 
+    @comp_move = @moves.sample 
   
     if 
       @comp_move == "rock"
@@ -15,5 +15,44 @@ class GameController < ApplicationController
     end
 
       render({ :template => "game_templates/play_rock" })
+  end
+
+  def paper
+    @moves = ["rock", "paper", "scissors"]
+    @comp_move = @moves.sample 
+  
+    if 
+      @comp_move == "rock"
+      @outcome = "won" 
+    elsif
+      @comp_move == "paper"
+      @outcome = "tied"
+    else
+      @outcome = "lost"
+    end
+
+    render({ :template => "game_templates/play_paper" })
+  end
+
+  def scissors
+    @moves = ["rock", "paper", "scissors"]
+    @comp_move = @moves.sample 
+  
+    if 
+      @comp_move == "rock"
+      @outcome = "lost" 
+    elsif
+      @comp_move == "paper"
+      @outcome = "won"
+    else
+      @outcome = "tied"
+    end
+
+    render({ :template => "game_templates/play_scissors" })
+  end
+
+  def home
+
+    render({ :template => "game_templates/home_page"})
   end
 end
