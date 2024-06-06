135361c18334953a473d85f60020016981e2f0bc
Ricardo Juarez <146899774+jrzricardo@users.noreply.github.com>
2024-06-06 20:23:21 +0000

    built rock, paper, and scissor outcome

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,19 @@
+class GameController < ApplicationController
+  def rock
+    moves = ["rock", "paper", "scissors"]
+  
+    @comp_move = moves.sample 
+  
+    if 
+      @comp_move == "rock"
+      @outcome = "tied" 
+    elsif
+      @comp_move == "paper"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+
+      render({ :template => "game_templates/play_rock" })
+  end
+end
